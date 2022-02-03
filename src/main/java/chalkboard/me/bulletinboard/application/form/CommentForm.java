1fe6de0d4ca928fbdb5e26e10b2ef76479327bd6
本馬遼太郎 <rajiku.blanc@gmail.com>
2022-02-03 10:49:57 +0900

    バリデーションの実装

--- src/main/java/chalkboard/me/bulletinboard/application/form/CommentForm.java
+++ src/main/java/chalkboard/me/bulletinboard/application/form/CommentForm.java
@@ -1,6 +1,11 @@
 package chalkboard.me.bulletinboard.application.form;
 
 import lombok.Data;
+import org.springframework.lang.Nullable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /*
     @Dataはコンストラクタとアクセサを自動で同時に作成するアノテーション。
@@ -9,7 +14,14 @@ import lombok.Data;
  */
 @Data
 public class CommentForm {
+    @Nullable
+    @Size(max = 20)
     private String name;
+    @Nullable
+    @Email
+    @Size(max = 100)
     private String email;
+    @NotNull
+    @Size(min = 1, max = 400)
     private String comment;
 }
--- src/main/java/chalkboard/me/bulletinboard/presentation/BoardController.java
+++ src/main/java/chalkboard/me/bulletinboard/presentation/BoardController.java
@@ -3,6 +3,8 @@ package chalkboard.me.bulletinboard.presentation;
 import chalkboard.me.bulletinboard.application.form.CommentForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,8 +21,13 @@ public class BoardController {
     }
 
     @PostMapping("/board")
-    public String postComment(@ModelAttribute CommentForm comment) {
+    public ModelAndView postComment(@Validated @ModelAttribute CommentForm comment, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            ModelAndView modelAndView = new ModelAndView("/board");
+            modelAndView.addObject("commentForm", comment);
+            return modelAndView;
+        }
 //        二重送信防止のためにredirectを記述する
-        return "redirect:board";
+        return new ModelAndView("redirect:board");
     }
 }
